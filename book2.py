511dc99056c96838f098b3317e742ddf12f13053
sangjinsu <climbplant39@kakao.com>
2022-12-15 22:57:35 +0900

    ✨ Add Books to BookList

---
+++ book2.py
@@ -0,0 +1,44 @@
+from uuid import UUID, uuid4
+from pydantic import BaseModel, Field
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=100)
+    description: str = Field(title="Description of the Book", min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=101)
+
+
+Books: [Book] = []
+
+
+@app.get("/")
+async def read_all_books() -> [Book]:
+    if len(Books) == 0:
+        Books.append(create_book_no_api())
+    return Books
+
+
+@app.post("/")
+async def create_book(book: Book) -> [Book]:
+    Books.append(book)
+    return Books
+
+
+def create_book_no_api() -> [Book]:
+    book_1 = Book(id=uuid4(),
+                  title="Title1",
+                  author="Author1",
+                  description="description1",
+                  rating=60)
+    book_2 = Book(id=uuid4(),
+                  title="Title2",
+                  author="Author2",
+                  description="description2",
+                  rating=60)
+    return [book_1, book_2]
