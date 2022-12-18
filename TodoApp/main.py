8c35aa8e4329f28882da18da39ee746d56eb20e9
sangjinsu <climbplant39@kakao.com>
2022-12-18 22:44:56 +0900

    :sparkles: get todo by id

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
+from starlette import status
+
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -19,3 +21,15 @@ def get_db():
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+
+@app.get("/{todo_id}")
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo is None:
+        http_exception()
+    return todo
+
+
+def http_exception():
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
