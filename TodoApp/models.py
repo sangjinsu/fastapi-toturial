6911ea77751c36ad607fa5a3aaf90de5c235e934
sangjinsu <climbplant39@kakao.com>
2022-12-19 17:52:33 +0900

    :sparkles: create db table users

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,8 +1,12 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from enum import unique
+
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 from database import Base
 
 
-class Todos(Base):
+class Todo(Base):
     __tablename__ = 'todos'
 
     id = Column(Integer, primary_key=True, index=True)
@@ -10,3 +14,20 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="todos")
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    todos = relationship("Todos", back_populates="owner")
